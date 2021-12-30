5c808ea90054771e4466aaa5bfe62437f53bd3a1
Wesleytyber <wesleytyber@gmail.com>
2021-12-30 18:08:07 -0300

    Initial Commit

---
+++ curso-tiago-aguiar-ios-app-habit/curso_tiago_aguiar_ios_app_habitApp.swift
@@ -0,0 +1,17 @@
+//
+//  curso_tiago_aguiar_ios_app_habitApp.swift
+//  curso-tiago-aguiar-ios-app-habit
+//
+//  Created by Wesley Calazans on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct curso_tiago_aguiar_ios_app_habitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
